2f689f5b8800b63d5408a433bb89338f7d5eaa15
MatthesP <m_podl01@wwu.de>
2016-05-22 17:32:19 +0200

    Add files via upload

---
+++ Twittertests/tweet_anleitung.ino
@@ -0,0 +1,42 @@
+
+/* Post a simple message to Twitter  */
+#if defined(ARDUINO) && ARDUINO > 18
+#include <SPI.h>
+#endif
+#include <Ethernet.h>
+//#include <EthernetDNS.h> // only for IDE 0022 or earlier
+#include <Twitter.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 192, 168, 1, 50 };
+byte gateway[] = { 192, 168, 1, 1 };
+byte subnet[] = { 255, 255, 255, 0 };
+
+Twitter twitter("734130075222839297-xCppwdtDIrZI6uoWok3dGMVVBjSczPT");
+char msg[] = "Hello, World! I'm Arduino!";
+
+void setup()
+{
+  Ethernet.begin(mac, ip, gateway, subnet);
+  Serial.begin(9600);
+
+  delay(1000);
+
+  Serial.println("connecting ...");
+  if (twitter.post(msg)) {
+    int status = twitter.wait(&Serial);
+    if (status == 200) {
+      Serial.println("OK.");
+    } else {
+      Serial.print("failed : code ");
+      Serial.println(status);
+    }
+  } else {
+    Serial.println("connection failed.");
+  }
+}
+
+void loop()
+{
+}
+
